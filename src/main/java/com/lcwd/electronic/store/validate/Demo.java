fcd2be0e5d5470f1a097ed7b0462ea81d8a89494
PranayDange <pranaydange04@gmail.com>
2023-03-13 20:31:17 +0530

    from mac

--- src/main/java/com/lcwd/electronic/store/validate/Demo.java
+++ src/main/java/com/lcwd/electronic/store/validate/Demo.java
@@ -2,7 +2,6 @@ package com.lcwd.electronic.store.validate;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.printf("Hello this project is about electronic store" +
-                "");
+        System.out.printf("Hello this project is about electronic store" + "");
     }
 }
