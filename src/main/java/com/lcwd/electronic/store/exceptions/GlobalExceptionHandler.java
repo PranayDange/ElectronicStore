c073131c43996a67d6b7e5db362ca92e4d111626
pranay.dange <pranay.dange@paycraftsol.com>
2023-03-13 08:12:38 +0530

    customizing paging and soring response

--- src/main/java/com/lcwd/electronic/store/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/lcwd/electronic/store/exceptions/GlobalExceptionHandler.java
@@ -45,4 +45,15 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
+
+
+    @ExceptionHandler(BadApiRequest.class)
+    public ResponseEntity<ApiResponseMessage> handleBadApiRequest(BadApiRequest ex) {
+
+        logger.info("Bad Api Request!!");
+
+        ApiResponseMessage response = ApiResponseMessage.builder().message(ex.getMessage()).status(HttpStatus.BAD_REQUEST).success(false).build();
+        return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+
+    }
 }
