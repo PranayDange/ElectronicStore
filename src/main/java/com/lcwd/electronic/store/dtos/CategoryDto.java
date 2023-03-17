47477e8439d93b8076d3edd1a30c4a016a06435d
PranayDange <pranaydange04@gmail.com>
2023-03-17 09:20:16 +0530

    Category entities and dtos created

---
+++ src/main/java/com/lcwd/electronic/store/dtos/CategoryDto.java
@@ -0,0 +1,27 @@
+package com.lcwd.electronic.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto {
+
+
+    private String categoryId;
+
+    @NotBlank
+    @Min(value = 4, message = "title must be of minimum 4 characters !! ")
+    private String title;
+    @NotBlank(message = "description required !!")
+    private String description;
+
+    private String coverImage;
+}
